aa06e6b540a7cd6e37f42af4223abd9c1736dd33
GenesisCuichan <genesis_cuichan@hotmail.com>
2022-07-04 08:59:43 -0500

    estilo agregado menu

--- Ejemplo_HTML-master/Ejemplo_HTML-master/Ejemplo_React/switch-state-demo/src/Components/Menu.jsx
+++ Ejemplo_HTML-master/Ejemplo_HTML-master/Ejemplo_React/switch-state-demo/src/Components/Menu.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 class Menu extends React.Component{
     render() {
         return (
-            <div>
-                <ul>
+            <div className='Menu'>
+                <ul className='lista'>
                     <li>Home</li>
                     <li>Products</li>
                     <li>Contact Us</li>
