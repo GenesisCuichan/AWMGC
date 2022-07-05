e8914eb0ae8e75feed4eccbce0249091a6e55bbf
GenesisCuichan <genesis_cuichan@hotmail.com>
2022-07-05 15:35:14 -0500

    aumentaredad

--- Ejemplo_HTML-master/Ejemplo_HTML-master/Ejemplo_React/react_hello_world/src/App.js
+++ Ejemplo_HTML-master/Ejemplo_HTML-master/Ejemplo_React/react_hello_world/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 import PersonCard from './Component/PersonCard'
@@ -6,11 +6,11 @@ import PersonCard from './Component/PersonCard'
 class App extends React.Component{
   render(){
     const data = [
-      {img: "https://estaticos.smoda.elpais.com/wp-content/uploads/2022/02/laurapausini1-1200x631.png", name:'Laura', city: 'Cuenca', country: 'Ecuador', address: 'García Moreno y Venezuela'},
-      {img: "https://i.ytimg.com/vi/JoqmHAr3fu8/maxresdefault.jpg", name:'Elvis', city: 'Loja', country: 'Ecuador', address: 'Pichincha y Venezuela'},
-      {img: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/EHPMKZIZOFC4RCIT3ZI7ENSNN4.png", name:'Luis', city: 'Guayaquil', country: 'Ecuador', address: 'Alonso de Angulo 23232'},
-      {img: "https://pbs.twimg.com/profile_images/753703360784658432/reSy9oNv_400x400.jpg", name:'Génesis', city: 'Guayaquil', country: 'Ecuador', address: 'Alonso de Angulo 23232'},
-      {img: "https://es.web.img3.acsta.net/pictures/20/11/27/06/28/4034475.jpg", name:'Joseph', city: 'Guayaquil', country: 'Ecuador', address: 'Alonso de Angulo 23232'}
+      {img: "https://cdn.pixabay.com/photo/2021/10/22/06/07/girl-6731188_960_720.png", name:'Laura', age: 1, city: 'Cuenca', country: 'Ecuador', address: 'García Moreno y Venezuela'},
+      {img: "https://img.freepik.com/vector-gratis/dibujos-animados-nina-feliz_43633-1438.jpg?w=2000",age: 1, name:'Genesis', city: 'Loja', country: 'Ecuador', address: 'Pichincha y Venezuela'},
+      {img: "https://i.pinimg.com/originals/6d/fd/f3/6dfdf34f15b8ba3882f8870b1c1d0061.png", name:'Sarah',age: 1, city: 'Guayaquil', country: 'Ecuador', address: 'Alonso de Angulo 23232'},
+      {img: "https://i.pinimg.com/736x/d6/95/31/d695313d53d7ae5a87ab0e6ad575318e.jpg", name:'Andrea',age: 1, city: 'Guayaquil', country: 'Ecuador', address: 'Alonso de Angulo 23232'},
+      {img: "https://i.pinimg.com/564x/b9/be/c8/b9bec836cde52404249e77471d80a404.jpg", name:'Josselin', age: 1,city: 'Guayaquil', country: 'Ecuador', address: 'Alonso de Angulo 23232'}
     ];
     return(
       <div className='App'>
@@ -18,7 +18,7 @@ class App extends React.Component{
         <PersonCard name={data[1].name} city={data[1].city} country={data[1].country} address={data[1].address}/>
     <PersonCard name={data[2].name} city={data[2].city} country={data[2].country} address={data[2].address}/>*/}
         {data.map((person,index) =>(
-          <PersonCard key={index} img={person.img} name={person.name} city={person.city} country={person.country} address={person.address}>
+          <PersonCard key={index} img={person.img} name={person.name} age={person.age} city={person.city} country={person.country} address={person.address}>
             <button onClick={()=>alert(`Contactando a ${person.name}`)}>{`Contactar a ${person.name}`}</button>
           </PersonCard>
         )
--- Ejemplo_HTML-master/Ejemplo_HTML-master/Ejemplo_React/react_hello_world/src/Component/PersonCard.jsx
+++ Ejemplo_HTML-master/Ejemplo_HTML-master/Ejemplo_React/react_hello_world/src/Component/PersonCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../style/PersonCard.css'
 
 class PersonCard extends React.Component{
     aceptarHandler(e){
@@ -10,13 +11,31 @@ class PersonCard extends React.Component{
     containerHandler(){
         console.log('Ejecutando componente padre');
     }
-    
+    //CONSTRUCTOR
+    constructor(props) {
+        super(props);
+       //Primer cambio
+       //el age de aqui es el q mando del . app
+        this.state = {edad: props.age};
+        //qUINTO CAMBIO
+        this.handlerAge = this.handlerAge.bind(this);
+    }
+    //metodo de clase
+    //metodo cambia edad TERCER CAMBIO
+    /*changeAge()*/
+    handlerAge(){
+        console.log(this.state.edad);
+        this.setState({edad: this.state.edad + 1})
+        //this.setState(prevState=>({count: parseInt(prevState.count) +1}));
+    }
+    //metodos de clase
     render(){
         console.log("Props de PersonCard", this.props);
+        //no  usamos el age aqui por que usamos el del estado que hemos creado
         const {name,city,country,address,img} = this.props; //Desestructuración
         
         return(
-            <div style={{backgroundColor:'yellow'}} onClick={this.containerHandler} className="container">
+            <div  onClick={this.containerHandler} className="container">
                 {/*<h1>{this.props.name}</h1>
                 <h3>{this.props.city}</h3>
                 <h3>{this.props.country}</h3>
@@ -24,12 +43,15 @@ class PersonCard extends React.Component{
                 <hr/>*/}
                 <img src={img} alt={name}/>
                 <h1>{name}</h1>
+                {/* cuarto Cambio */}
+                <h3>{this.state.edad}</h3>
                 <h3>{city}</h3>
                 <h3>{country}</h3>
                 <h5>{address}</h5>
+                {/* Segundo Cambio */}
+                <button onClick={this.handlerAge}>Aumentar edad de {name}</button>
                 <button onClick={this.aceptarHandler}>Aceptar</button>
                 {this.props.children}
-                <hr/>
             </div>
         );
     }
